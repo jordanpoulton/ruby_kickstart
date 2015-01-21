0e0c2553f980ab68c29dfe2cbe98ba2a34b4199e
Meads <meads109@gmail.com>
2015-01-21 14:42:44 +0000

    my solutions session1 moved into Jordans fork

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -1,6 +1,6 @@
 # A grad student at a local university thinks he has discovered a formula to
-# predict what kind of grades a person will get. He says if you own less than
-# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C",
+# predict what kind of grades a person will get. He says if you own less than 
+# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C", 
 # and if you own more than 20 books, you will get a "B".
 # He further hypothesizes that if you actually read your books, then you will
 # get a full letter grade higher in every case.
@@ -10,5 +10,21 @@
 # grade(15, true)   # => "B"
 
 def grade(num_books, reads_books)
-
+	if reads_books == true
+		if num_books < 10 then 
+			"C"
+		elsif num_books <= 20 then
+			"B"
+		else
+			"A"
+		end
+	else 		
+		if num_books < 10 then
+			"D"
+		elsif num_books <= 20 then
+			"C"
+		else
+			"B"		
+		end
+	end
 end
--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,4 +5,14 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	result = ""
+	len = string.size - 1
+	len.times do |index|
+		if string[index] == "r" || string[index] == "R" 
+			result << string[index + 1]
+		end
+	end
+	result
 end
+
+
