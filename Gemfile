b584aaa77b4955dac13e4530d3510d449851f858
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-01-21 11:03:55 +0000

    Updated too many files :/

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'rspec', '~> 3.0'
 gem 'rake'
+gem 'sinatra'
--- session1/challenge/5_string.rb
+++
@@ -1,8 +0,0 @@
-# Given a string, replace every instance of sad to happy
-#
-
-def add_more_ruby(string)
-  string.gsub!(/sad/, "happy")
-  string.gsub!(/Sad/, "Happy")
-  string
-end
