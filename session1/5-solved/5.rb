eec91641b174bf8898a3a2152e873e81a265e06e
SebastienPires <pires.sebastien@gmail.com>
2015-01-22 22:38:14 +0000

    Added Sebastien answer

--- session1/5-solved/5.rb
+++ session1/5-solved/5.rb
@@ -14,3 +14,8 @@ def add_more_ruby(string)
 	string.gsub(/sad/, "happy").gsub(/Sad/, "Happy")
 end
 #======================
+
+#Sebastien
+def add_more_ruby(string)
+	string.gsub("sad", "happy").gsub("Sad", "Happy")
+end
