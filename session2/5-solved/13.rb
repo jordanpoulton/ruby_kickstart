f49967bb3250072e955d53c52c9b80471c50f6d1
SebastienPires <pires.sebastien@gmail.com>
2015-01-23 17:48:58 +0000

    Updated Sebastien answer

--- session2/5-solved/13.rb
+++ session2/5-solved/13.rb
@@ -48,38 +48,43 @@ class User
   attr_accessor :username, :blogs
 
   def initialize(username)
-    self.username = username
-    self.blogs    = []
+    self.username, self.blogs = username, Array.new
   end
 
   def add_blog(date, text)
-    added_blog = Blog.new(date, self, text)
-    blogs << added_blog
-    self.blogs = blogs.sort_by { |blog| blog.date }.reverse
-    added_blog
+    Blog.new date, self, text
   end
+
+  def blogs
+     @blogs.sort! { |blog1, blog2| blog2.date <=> blog1.date }
+  end
+
 end
 
 class Blog
   attr_accessor :date, :user, :text
 
   def initialize(date, user, text)
-    self.date = date
-    self.user = user
-    self.text = text
+    self.date, self.user, self.text = date, user, text
+    user.blogs << self
   end
 
   def summary
-    text.split[0..9].join(' ')
+    summary = ""
+    get = @text.split(/ /)
+    get.each_index do |word|
+      summary << get[word] << " " if word < 10
+    end
+    summary.strip
   end
 
-  def entry
-    "#{user.username} #{date}\n#{text}"
+  def display_entry
+    puts user.username + " " + date.to_s
+    puts text
   end
-
   def ==(other)
-    date   == other.date &&
-      user == other.user &&
-      text == other.text
+    self.date == other.date && 
+    self.user == other.user && 
+    self.text == other.text
   end
 end
