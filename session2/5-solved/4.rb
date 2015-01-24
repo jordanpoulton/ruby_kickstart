c85a3c100426afdb206fd9ed33954afba3193995
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-01-24 15:49:48 +0000

    My solutions to 2:4

--- session2/5-solved/4.rb
+++ session2/5-solved/4.rb
@@ -1,3 +1,8 @@
+#Josh
+def get_squares(numbers)
+  numbers.select { |n| numbers.include? n*n }.sort
+end
+
 #paul fitz
 def get_squares(numbers)
 	answer = []									#create an empty array to store the answers
@@ -5,10 +10,6 @@ def get_squares(numbers)
 		answer << x if numbers.include?(x*x)	#add the number to the answers array if the the square of the number is included
 end
 answer.sort										#sort the answers array
-end
-
-def get_squares(numbers)
-  numbers.select { |n| numbers.include? n*n }.sort
 end
 
 #solved by meads
@@ -26,4 +27,9 @@ end
 
 def get_squares(n)
   n.select { |x| n.include? x*x }.sort
+end
+
+#Jordan
+def get_squares(array)
+  array.sort.select {|num| array.include? num*num}
 end
