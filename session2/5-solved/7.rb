5e3434cdee02b96f322a127987cd1f658795cae6
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-01-24 17:15:09 +0000

    Solved 7

--- session2/5-solved/7.rb
+++ session2/5-solved/7.rb
@@ -11,12 +11,24 @@ def alternate_words(sentence)
   solution
 end
 
+#Jordan
+def alternate_words(string)
+  array = string.gsub(/[!@$#%^&*()-=_+\[\]:;,.\/<>?\|]/, " ").split
+
+  solution = Array.new
+  array.each_with_index do |word, index|
+    solution << word if index.even?
+  end
+
+  solution
+end
+
 #solved by meads
 def alternate_words string
 	arr = string.gsub(/[^\w'\u2019]/,' ').split(' ')#with [^..] anything inside the brackets is excluded and \w is all word characters.
 	new = arr.each_index.select{|x| x.even?}
 	concat = new.map {|x| arr[x]}
-	return concat 
+	return concat
 end
 #The editor is used doesn't support UTF-8 characters, the unicode number had to be used
 #in the regex to pick up apostrophes over single quotes.
