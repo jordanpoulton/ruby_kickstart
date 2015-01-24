1c30d1310a40ecb906483011921e2b51ee42fbd2
Meads <meads109@gmail.com>
2015-01-24 19:35:07 +0000

    q11 bottles of beer completed

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -22,82 +22,57 @@
 
 class BeerSong
 	def initialize numBottles
-		@numBottles = numBottles	
+		@numBottles = numBottles#this line must be first else it will override @numbottles
+    @numBottles = 0  if numBottles < 0
+    @numBottles = 99 if numBottles > 99
+
 		@ones = {0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine'}
 		@tens = {10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen',
 						17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen'}
-		@tys = {2 => 'twenty', 3 => 'thirty', 4 => 'fourty', 5 => 'fifthy', 6 => 'sixty', 7 => 'seventy', 8 => 'enighty', 9 => 'ninety'}
+		@tys = {2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
 	end
 	
 	
 		
 	def createWordNumber num
-		if num < 10 
-			return @ones[num] 
-		elsif num < 20
-			return @tens[num] 
-		else 
-			twentyAndAbove num
-		end
+		return @ones[num] if num < 10 
+		return @tens[num] if num < 20
+		return	twentyAndAbove num
 	end
 		
-	def twentyAndAbove num
-		tens, ones = num.divmod(10)
+	def twentyAndAbove num #combines the @tys array and @ones array to make the numbers 20 and over.
+		tens, ones = num.divmod(10)#divides num by 10 and assigns the quotient to 'tens' and the modulus to 'ones'.
 		return "#{@tys[tens]}" if ones == 0
-		return "#{@tys[tens]}#{@ones[ones]}"
-	end
-	
-	
-	def outSideCase
-		if @numBottles < 0 
-			@numBottles = 0
-		elsif @numBottles > 99
-			@numBottles = 99 
-		end
+		return "#{@tys[tens]}-#{@ones[ones]}"
 	end
 
-def bottles numBottles
-	 if numBottles == 1
+
+def bottles bottles
+	 if bottles == 1
 		 return "bottle"
 	 else
 		 return "bottles" 
 	 end
 end
 
-	def all_stanzaz
-		outSideCase
-		return "" if @numBottles == 0
-		@numBottles.downto(0) do |num|
+def print_song
+		return String.new if @numBottles.zero?
+		@numBottles.downto(1) do |num|
 			puts"#{createWordNumber(num).capitalize} #{bottles(num)} of beer on the wall,"
 		  puts"#{createWordNumber(num).capitalize} #{bottles(num)} of beer,"
-		puts"Take one down, pass it around,"
-			if num > 0
-				puts"#{createWordNumber(num-1).capitalize} #{bottles(num-1)} of beer on the wall."
+			puts"Take one down, pass it around,"
+			if num == 1
+				print"#{createWordNumber(num-1).capitalize} #{bottles(num-1)} of beer on the wall."
 			else
-				puts"#{createWordNumber(num)} #{bottles(num)}  of beer on the wall." #handle the last case of zero bottles
+				puts"#{createWordNumber(num-1).capitalize} #{bottles(num-1)} of beer on the wall."
 			end
 		end
 	end
 	
-
-def one_stanza
-		outSideCase
-		return "" if @numBottles == 0
-		puts"#{createWordNumber(@numBottles).capitalize} #{bottles(@numBottles)} of beer on the wall,"
-	  puts"#{createWordNumber(@numBottles).capitalize} #{bottles(@numBottles)} of beer,"
-		puts"Take one down, pass it around,"
-			if @numBottles > 0
-				puts"#{createWordNumber(@numBottles-1).capitalize} #{bottles(@numBottles-1)} of beer on the wall."
-			else
-				puts"#{createWordNumber(@numBottles)} #{bottles(@numBottles)}  of beer on the wall." #handle the last case of zero bottles
-			end
-	end
-	
-	
 end
 
-b = BeerSong.new(99)
- b.print_song
+#b = BeerSong.new(100)
+ #b.print_song
 #puts b.createWordNumber(15)
 #puts b.instance_variable_get '@numBottles'
 
