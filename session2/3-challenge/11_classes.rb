f513b6f371424654feb8982323b6f967177c7599
Paul Fitzgerald <paulfitz99@gmail.com>
2015-01-25 11:36:21 +0000

    fix

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-class BeerSong					#create a class BeerSong
-  attr_accessor :beers 			#create attr_accessor so the beer object can be updated
-
-  def initialize(beers)			#sets up values of the object
-    beers = 0  if beers < 0
-    beers = 99 if beers > 99
-    @beers = beers
-  end
- 
- def translate(n)				#translate numbers into their corresponding matuching number string
-    if 0 <= n && n <= 19
-      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
-    elsif n % 10 == 0
-      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
-    else
-      "#{translate n/10*10}-#{translate n%10}".downcase
-    end.capitalize
-  end
-
-	def print_verse(n)					#prints verse, if n == 0
-		if n == 0
-			puts ""
-		else
-			puts "#{translate n} #{bottle n} of beer on the wall,"
-			puts "#{translate n} #{bottle n} of beer,"
-			puts "Take one down, pass it around,"
-			puts "#{translate n-1} #{bottle n-1} of beer on the wall."
-		end
-	end
-
-	def bottle(n)
-		n == 1 ? "bottle" : "bottles"
-	end
-
-	def print_song
-		beers.downto 1 do |num|
-		print_verse(num)
-	end
-end
-end
-
-
-
-
-
-=======
 # Write a program that outputs the lyrics for "Ninety-nine Bottles of Beer on the Wall"
 # Your program should print the number of bottles in English, not as a number. For example:
 #
@@ -69,4 +22,6 @@ end
 class BeerSong
 end
 
->>>>>>> 957a49424f53cb1f9a72406f6abc7d78c4fb0057
+
+
+
