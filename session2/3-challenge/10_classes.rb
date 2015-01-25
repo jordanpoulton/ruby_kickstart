2d1f927138549c056add6131c0017d9601638735
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-01-25 14:55:41 +0000

    Cleaned up

--- session2/3-challenge/10_classes.rb
+++ session2/3-challenge/10_classes.rb
@@ -17,22 +17,6 @@
 
 class Person
 
-  attr_accessor :name, :age
-
-  def initialize(name, age)
-  @name = name
-  @age = age
-  end
-
-  def birthday
-    @age += 1
-  end
 end
 
 
-guy = Person.new 'Fred', 61
-
-puts guy.name
-puts guy.birthday
-guy.name = 'Frederick'
-puts guy.name
