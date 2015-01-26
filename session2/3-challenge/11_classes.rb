ec871c16fa7081c22b310fdac45a15312565cca1
meads58 <meads109@gmail.com>
2015-01-26 18:17:58 +0000

    move session2 q11 and q12 to solved

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -12,7 +12,7 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
@@ -20,56 +20,7 @@
 # Add any additional methods you find helpful.
 
 
-class BeerSong
-	def initialize numBottles
-		@numBottles = numBottles#this line must be first else it will override @numbottles
-    @numBottles = 0  if numBottles < 0
-    @numBottles = 99 if numBottles > 99
 
-		@ones = {0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine'}
-		@tens = {10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen',
-						17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen'}
-		@tys = {2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
-	end
-	
-	
-		
-	def createWordNumber num
-		return @ones[num] if num < 10 
-		return @tens[num] if num < 20
-		return	twentyAndAbove num
-	end
-		
-	def twentyAndAbove num #combines the @tys array and @ones array to make the numbers 20 and over.
-		tens, ones = num.divmod(10)#divides num by 10 and assigns the quotient to 'tens' and the modulus to 'ones'.
-		return "#{@tys[tens]}" if ones == 0
-		return "#{@tys[tens]}-#{@ones[ones]}"
-	end
-
-
-def bottles bottles
-	 if bottles == 1
-		 return "bottle"
-	 else
-		 return "bottles" 
-	 end
-end
-
-def print_song
-		return String.new if @numBottles.zero?
-		@numBottles.downto(1) do |num|
-			puts"#{createWordNumber(num).capitalize} #{bottles(num)} of beer on the wall,"
-		  puts"#{createWordNumber(num).capitalize} #{bottles(num)} of beer,"
-			puts"Take one down, pass it around,"
-			if num == 1
-				print"#{createWordNumber(num-1).capitalize} #{bottles(num-1)} of beer on the wall."
-			else
-				puts"#{createWordNumber(num-1).capitalize} #{bottles(num-1)} of beer on the wall."
-			end
-		end
-	end
-	
-end
 
 #b = BeerSong.new(100)
  #b.print_song
--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -20,32 +20,5 @@
 # f.to_s               # => "50/100"
 # f.to_f               # => 0.5
 
-class Fraction
-	attr_accessor :numerator, :denominator
-	
-	def initialize (numerator, denominator)
-		@denominator = denominator
-		@numerator = 	numerator
-	end
-	
-  def gcd(a,b)
-    return a if b == 0
-		gcd(b, a%b)#recursion being used here.
-  end
-	
-	def to_s#this is overriding the usualy to_s method with this one hence puts is using this to_s
-		"#{@numerator}/#{@denominator}"
-	end
-	
-	def to_f
-		@numerator.to_f / @denominator.to_f
-	end
-	
-	def lowest
-		gcommon = gcd(@numerator, @denominator)
-		Fraction.new(@numerator / gcommon,@denominator / gcommon)
-	end
-	
-end
 
 
