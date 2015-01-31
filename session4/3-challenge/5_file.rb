d2b188e36fad2915a88ba62d62254826e1c4108c
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-01-31 10:11:30 +0000

    Solve 4:5

--- session4/3-challenge/5_file.rb
+++ session4/3-challenge/5_file.rb
@@ -5,13 +5,20 @@
 #
 # EXAMPLE
 #     file: nums.txt
-#      406 217 799 116 45 651 808 780 
-#      205 919 474 567 712 
-#      776 170 681 86 822 9 100 540 812 
-#      602 117 181 169 876 336 
+#      406 217 799 116 45 651 808 780
+#      205 919 474 567 712
+#      776 170 681 86 822 9 100 540 812
+#      602 117 181 169 876 336
 #      434 165 725 187 974 48
 #
 # line_sums('nums.txt')   # =>   808 + 919 + 822 + 876 + 974   # =>   4399
 
-def line_sums
+def line_sums(file)
+  highest_on_each_line = []
+
+  File.readlines(file).each do |line|
+      highest_on_each_line << line.split.map{|e| e.to_i}.max
+  end
+
+  highest_on_each_line.inject(0){|sum, x| sum + x.to_i}
 end
