72e9de7963a624a16809cb80442deb17858fd749
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-01-31 10:13:01 +0000

    Refactor 4:5

--- session4/5-solved/5.rb
+++ session4/5-solved/5.rb
@@ -13,6 +13,6 @@ def line_sums(file)
       highest_on_each_line << line.split.map{|e| e.to_i}.max
   end
 
-  highest_on_each_line.inject(0){|sum, x| sum + x.to_i}
+  highest_on_each_line.inject(0){|sum, x| sum + x}
 end
 #======
