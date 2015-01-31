1b612edcf42e8873d3f260492211e5dee4a66c5e
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-01-31 10:30:04 +0000

    Solve 4:7

--- session4/5-solved/7.rb
+++ session4/5-solved/7.rb
@@ -1,3 +1,9 @@
 def longest_method(object)
   object.methods.max_by { |meth| meth.length }
 end
+
+#Jordan
+def longest_method(object)
+  object.methods.sort {|a,b| a.length <=> b.length}.pop
+end
+#======
