be3020f0031935d35c5a6d6b78101730422d76eb
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-02-01 20:53:19 +0000

    Moved instructions

--- session6/3-challenge/1_build_an_app/main.rb
+++ session6/3-challenge/1_build_an_app/main.rb
@@ -1,14 +1,3 @@
-# Put under source control
-# Create the Gemfile
-# Add Rspec, Sinatra and Capybara
-# Install Gems
-# $ bundle exec rspec spec
-# Write the Sinatra Application
-# Add rackup
-# Show that it works locally
-# Commit everything to git
-# Create a new app on heroku
-# Push the app to heroku
 require 'sinatra'
 require File.dirname(__FILE__)+'/lib/caesar_cipher'
 
