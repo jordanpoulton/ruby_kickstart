abc18cbc982db3fad61c950ed4a5cf62f9a0a1ce
Chris Ward <chriswarddev@gmail.com>
2015-02-01 19:04:10 +0000

    session 2 challenge 11 solved. challenge file cleaned and soution appended to solve file

--- session2/5-solved/11.rb
+++ session2/5-solved/11.rb
@@ -518,3 +518,83 @@ class BeerSong
     end
 end
 
+#Chris Ward
+
+class BeerSong
+
+  attr_accessor 'bottles'
+
+  def initialize(bottles)
+    if bottles < 0
+      bottles = 0
+    elsif bottles > 99
+      bottles = 99
+    else 
+      bottles = bottles
+    end
+    @bottles = bottles
+  end
+
+  def English_number number
+    return "Zero" if number == 0
+    numString = ''  # This is the string we will return.
+      onesPlace = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+      tensPlace = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+      teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+        left = number
+        write = left/10          # How many tens left to write out?
+      left  = left - write*10  # Subtract off those tens.
+      if write > 0
+        if ((write == 1) and (left > 0))
+          # Since we can't write "tenty-two" instead of "twelve",
+          # we have to make a special exception for these.
+          numString = numString + teenagers[left-1]
+          # The "-1" is because teenagers[3] is 'fourteen', not 'thirteen'.
+
+          # Since we took care of the digit in the ones place already,
+          # we have nothing left to write.
+          left = 0
+        else
+          numString = numString + tensPlace[write-1]
+          # The "-1" is because tensPlace[3] is 'forty', not 'thirty'.
+        end
+
+        if left > 0
+          # So we don't write 'sixtyfour'...
+          numString = numString + '-'
+        end
+    end
+    write = left  # How many ones left to write out?
+      left  = 0     # Subtract off those ones.
+    if write > 0
+        numString = numString + onesPlace[write-1]
+        # The "-1" is because onesPlace[3] is 'four', not 'three'.
+      end
+    # Now we just return "numString"...
+      numString
+  end
+
+
+  def print_song 
+    count = @bottles
+    return if count == 0
+    while count > 1
+      puts "#{English_number(count).capitalize} bottles of beer on the wall,"
+      puts "#{English_number(count).capitalize} bottles of beer,"
+      puts "Take one down, pass it around,"
+      count = count-1
+      if count == 1
+        puts "One bottle of beer on the wall."
+        break
+      end
+      puts "#{English_number(count).capitalize} bottles of beer on the wall."
+    end
+    puts "One bottle of beer on the wall,"  
+    puts "One bottle of beer,"
+    puts "Take one down, pass it around,"
+    puts "Zero bottles of beer on the wall."
+  end
+
+end
+
+
